b70ba7255ce2b6ab1b3b5659c84f961846f61f2c
AdrianGalindoLlacer <adriangalindollacer@gmail.com>
2026-02-08 14:18:44 +0100

    Movimiento con rigidbody hecho

--- Prueba Movimiento enemigos/Assets/_Root_Prueba_Enemigos/Scripts/PlayerMovement.cs
+++ Prueba Movimiento enemigos/Assets/_Root_Prueba_Enemigos/Scripts/PlayerMovement.cs
@@ -113,15 +113,5 @@ public class PlayerMovement : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
     }
 
-    public void OnFire(InputAction.CallbackContext context)
-    {
-
-    }
-
-    public void OnDash(InputAction.CallbackContext context)
-    {
-
-    }
-
     #endregion
 }
